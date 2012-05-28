3709f259a54f48b564f46a52480246e1d87061f0
Paul Jenkins <paul@jenkinsfamily.com.au>
2012-05-28 14:52:56 +1000

    Now handles different date modes (full names , abbreviated names or using numbers)

--- MahApps.Metro/Controls/DateTimePicker/DatePicker.cs
+++ MahApps.Metro/Controls/DateTimePicker/DatePicker.cs
@@ -23,8 +23,22 @@ using System.Windows.Controls;
 
 namespace MahApps.Metro.Controls.DateTimePicker
 {
+    public enum DateMode
+    {
+        Full,
+        Abbreviated,
+        Numbers
+    }
+
     public class DatePicker : DateTimePickerBase
     {
+        public static readonly DependencyProperty ModeProperty = DependencyProperty.Register("Mode", typeof(DateMode), typeof(DatePicker), new PropertyMetadata(DateMode.Full));
+        public DateMode Mode
+        {
+            get { return (DateMode)GetValue(ModeProperty); }
+            set { SetValue(ModeProperty, value); }
+        }
+
         private ComboBox _primarySelector;
         private ComboBox _secondarySelector;
         private ComboBox _tertiarySelector;
@@ -43,7 +57,20 @@ namespace MahApps.Metro.Controls.DateTimePicker
 
             var info = new DateTimeFormatInfo();
 
-            months = info.MonthNames.ToList();
+            switch (Mode)
+            {
+                case DateMode.Full:
+                    months = info.MonthNames.ToList();
+                    break;
+                case DateMode.Abbreviated:
+                    months = info.AbbreviatedMonthNames.ToList();
+                    break;
+                case DateMode.Numbers:
+                    for (int i = 1; i <= 12; i++)
+                        months.Add(i.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0'));
+                    break;
+            }
+
             if (months.Count == 13)
                 months.RemoveAt(12);
 
@@ -71,13 +98,32 @@ namespace MahApps.Metro.Controls.DateTimePicker
             var selected = _secondarySelector.SelectedItem.ToString();
             int month = -1;
 
-            for (var i = 0; i < CultureInfo.CurrentCulture.DateTimeFormat.MonthNames.Count(); i++)
+            switch (Mode)
             {
-                if (selected != CultureInfo.CurrentCulture.DateTimeFormat.MonthNames[i])
-                    continue;
+                case DateMode.Full:
+                    for (var i = 0; i < CultureInfo.CurrentCulture.DateTimeFormat.MonthNames.Count(); i++)
+                    {
+                        if (selected != CultureInfo.CurrentCulture.DateTimeFormat.MonthNames[i])
+                            continue;
 
-                month = i + 1;
-                break;
+                        month = i + 1;
+                        break;
+                    }
+                    break;
+
+                case DateMode.Abbreviated:
+                    for (var i = 0; i < CultureInfo.CurrentCulture.DateTimeFormat.AbbreviatedMonthNames.Count(); i++)
+                    {
+                        if (selected != CultureInfo.CurrentCulture.DateTimeFormat.AbbreviatedMonthNames[i])
+                            continue;
+
+                        month = i + 1;
+                        break;
+                    }
+                    break;
+                case DateMode.Numbers:
+                    month = Int32.Parse(selected);
+                    break;
             }
 
             var days = new List<string>();
